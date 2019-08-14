e69d46407bfc24945c5e19645b68670c529e331f
Sébastien Doeraene <sjrdoeraene@gmail.com>
2019-08-14 17:51:34 +0200

    WiP Make all the RichXyz extensions self-contained.
    So that they do not inherit methods from universal traits, which
    causes unexpected boxing, sometimes even going through `Ordering`s
    for no reason whatsoever.

--- src/library/scala/runtime/RichInt.scala
+++ src/library/scala/runtime/RichInt.scala
@@ -15,30 +15,88 @@ package runtime
 
 import scala.collection.immutable.Range
 
-// Note that this does not implement IntegralProxy[Int] so that it can return
-// the Int-specific Range class from until/to.
-final class RichInt(val self: Int) extends AnyVal with ScalaNumberProxy[Int] with RangedProxy[Int] {
-  protected def num = scala.math.Numeric.IntIsIntegral
-  protected def ord = scala.math.Ordering.Int
+final class RichInt(private val self: Int) extends AnyVal {
+  @deprecated("statically known to be true", "2.14.0")
+  def isWhole: Boolean = true
 
-  override def doubleValue = self.toDouble
-  override def floatValue  = self.toFloat
-  override def longValue   = self.toLong
-  override def intValue    = self
-  override def byteValue   = self.toByte
-  override def shortValue  = self.toShort
+  @deprecated("use the method available on Int itself", "2.14.0")
+  def toChar: Char = self.toChar
+  @deprecated("use the method available on Int itself", "2.14.0")
+  def toByte: Byte = self.toByte
+  @deprecated("use the method available on Int itself", "2.14.0")
+  def toShort: Short = self.toShort
+  @deprecated("use the method available on Int itself", "2.14.0")
+  def toInt: Int = self.toInt
+  @deprecated("use the method available on Int itself", "2.14.0")
+  def toLong: Long = self.toLong
+  @deprecated("use the method available on Int itself", "2.14.0")
+  def toFloat: Float = self.toFloat
+  @deprecated("use the method available on Int itself", "2.14.0")
+  def toDouble: Double = self.toDouble
 
-  /** Returns `'''true'''` if this number has no decimal component.
-    * Always `'''true'''` for `RichInt`.
+  @deprecated("use toByte instead", "2.14.0")
+  def byteValue: Byte = self.toByte
+  @deprecated("use toShort instead", "2.14.0")
+  def shortValue: Short = self.toShort
+  @deprecated("statically known to be an identity", "2.14.0")
+  def intValue: Int = self
+  @deprecated("use toLong instead", "2.14.0")
+  def longValue: Long = self.toLong
+  @deprecated("use toFloat instead", "2.14.0")
+  def floatValue: Float = self.toFloat
+  @deprecated("use toDouble instead", "2.14.0")
+  def doubleValue: Double = self.toDouble
+
+  /** Returns `true` iff this is within the range of [[scala.Byte]] MinValue
+    * and MaxValue; otherwise returns `false`.
     */
-  def isWhole = true
+  def isValidByte: Boolean = self.toByte.toInt == self
 
-  override def isValidInt   = true
-  def isValidLong  = true
+  /** Returns `true` iff this is within the range of [[scala.Short]] MinValue
+    * and MaxValue; otherwise returns `false`.
+    */
+  def isValidShort: Boolean = self.toShort.toInt == self
 
-  override def abs: Int            = math.abs(self)
-  override def max(that: Int): Int = math.max(self, that)
-  override def min(that: Int): Int = math.min(self, that)
+  /** Returns `true`. */
+  @deprecated("statically known to be true", "2.14.0")
+  def isValidInt: Boolean = true
+
+  /** Returns `true` iff this is within the range of [[scala.Char]] MinValue
+    * and MaxValue; otherwise returns `false`.
+    */
+  @deprecated("statically known to be true", "2.14.0")
+  def isValidChar: Boolean = self.toChar.toInt == self
+
+  /** Result of comparing `this` with operand `that`.
+   *
+   *  Returns `x` where:
+   *
+   *  - `x < 0` when `this < that`
+   *  - `x == 0` when `this == that`
+   *  - `x > 0` when  `this > that`
+   */
+  def compare(y: Int): Int = Integer.compare(self, y)
+
+  /** Returns `'''this'''` if `'''this''' < that` or `that` otherwise. */
+  def min(that: Int): Int = Math.min(self, that)
+
+  /** Returns `'''this'''` if `'''this''' > that` or `that` otherwise. */
+  def max(that: Int): Int = Math.max(self, that)
+
+  /** Returns the absolute value of `'''this'''`. */
+  def abs: Int = Math.abs(self)
+
+  /**
+   * Returns the sign of `'''this'''`.
+   * zero if the argument is zero, -zero if the argument is -zero,
+   * one if the argument is greater than zero, -one if the argument is less than zero,
+   * and NaN if the argument is NaN where applicable.
+   */
+  def sign: Int = Integer.signum(self)
+
+  /** Returns the signum of `'''this'''`. */
+  @deprecated("use `sign` method instead", since = "2.13.0")
+  def signum: Int = sign
 
   /** There is no reason to round an `Int`, but this method is provided to avoid accidental loss of precision from a detour through `Float`. */
   @deprecated("this is an integer type; there is no reason to round it.  Perhaps you meant to call this on a floating-point value?", "2.11.0")
@@ -48,6 +106,7 @@ final class RichInt(val self: Int) extends AnyVal with ScalaNumberProxy[Int] wit
   def toHexString: String    = java.lang.Integer.toHexString(self)
   def toOctalString: String  = java.lang.Integer.toOctalString(self)
 
+  @deprecated("use Range instead", "2.14.0")
   type ResultWithoutStep = Range
 
   /**
@@ -80,4 +139,6 @@ final class RichInt(val self: Int) extends AnyVal with ScalaNumberProxy[Int] wit
     *         and including `end`.
     */
   def to(end: Int, step: Int): Range.Inclusive = Range.inclusive(self, end, step)
+
+  override def toString(): String = java.lang.Integer.toString(self)
 }
